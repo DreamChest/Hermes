9f40707253014827e47f606c92ce132e201a9bc6
Quentin Sonrel <sudiukil@gmx.fr>
2018-03-20 16:00:33 +0100

    Added base JWT auth

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :tags
+
+  post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

0dcea9ded11e43e43959d47d1ba4f942ba4426ef
Quentin Sonrel <sudiukil@gmx.fr>
2018-03-20 14:07:31 +0100

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  resources :sources do
+    resources :articles, only: %i[show index] do
+      resource :content, only: :show
+    end
+    resources :tags, only: %i[show index]
+  end
+
+  resources :articles, only: %i[show index] do
+    resource :content, only: :show
+  end
+
   resources :tags
-  resources :contents
-  resources :articles
-  resources :sources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

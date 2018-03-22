83da43cd5dd2431bed2bbe016d6f17f58ee8be95
Quentin Sonrel <sudiukil@gmx.fr>
2018-03-22 15:40:04 +0100

    Altered user model and added admin account to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20180320133748) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false

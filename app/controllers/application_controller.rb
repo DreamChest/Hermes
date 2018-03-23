7a7a3c1843dd11d353f3213ac9aa68dbe5adae11
Quentin Sonrel <sudiukil@gmx.fr>
2018-03-23 16:46:12 +0100

    Added code documentation for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+# Main controller class
 class ApplicationController < ActionController::API
   before_action :authenticate_request
   attr_reader :current_user
 
   private
 
+  # Authenticate incoming requests
   def authenticate_request
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user

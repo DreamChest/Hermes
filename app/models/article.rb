fa29ca0816a9af0488ce7c574d47aa217a33b473
Sudiukil <sudiukil@gmx.fr>
2018-04-03 01:15:07 +0200

    Added some code documentation to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,9 @@ class Article < ApplicationRecord
 
   validates :title, :date, :url, presence: true
 
+  # Filter Articles by Sources
+  # @param sources Sources to filter by (array of Source names (strings))
+  # @return [Array] array of articles filtered by Sources
   def self.filter_by_source(sources)
     Source
       .where('name in (?)', sources)
@@ -13,6 +16,9 @@ class Article < ApplicationRecord
       .flatten
   end
 
+  # Filter Articles by Tags
+  # @param tags Tags to filter by (array of Tag names (strings))
+  # @return [Array] array of Articles filtered by Tags
   def self.filter_by_tags(tags)
     Tag
       .where('name in (?)', tags)

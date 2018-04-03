c3e00c6b8e25242cd87b751cd872fb1d2791dea5
Quentin Sonrel <sudiukil@gmx.fr>
2018-04-03 11:53:11 +0200

    Added versionning to API

---
+++ app/controllers/v1/authentication_controller.rb
@@ -0,0 +1,17 @@
+module V1
+  # Authentication controller class (for API)
+  class AuthenticationController < ApplicationController
+    skip_before_action :authenticate_request
+
+    # Authenticate a user based on email and password
+    def authenticate
+      command = AuthenticateUser.call(params[:email], params[:password])
+
+      if command.success?
+        render json: { auth_token: command.result }
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,32 @@
 Rails.application.routes.draw do
-  get '/sources/update', to: 'sources#update_all'
-  resources :sources do
+  namespace :v1 do
+    get '/sources/update', to: 'sources#update_all'
+    resources :sources do
+      resources :articles, only: %i[show index] do
+        resource :content, only: :show
+      end
+
+      resources :tags, only: %i[show index]
+    end
+
+    get '/sources/:id/update', to: 'sources#update_articles'
+    get '/sources/:id/clear', to: 'sources#clear'
+    get '/sources/:id/reset', to: 'sources#reset'
+
     resources :articles, only: %i[show index] do
       resource :content, only: :show
     end
 
-    resources :tags, only: %i[show index]
-  end
-
-  get '/sources/:id/update', to: 'sources#update_articles'
-  get '/sources/:id/clear', to: 'sources#clear'
-  get '/sources/:id/reset', to: 'sources#reset'
-
-  resources :articles, only: %i[show index] do
-    resource :content, only: :show
-  end
-
-  get '/tags/clean', to: 'tags#clean'
-  resources :tags do
-    resources :sources, only: %i[show index] do
-      resources :articles, only: %i[show index] do
-        resource :content, only: :show
+    get '/tags/clean', to: 'tags#clean'
+    resources :tags do
+      resources :sources, only: %i[show index] do
+        resources :articles, only: %i[show index] do
+          resource :content, only: :show
+        end
       end
     end
-  end
 
-  post 'authenticate', to: 'authentication#authenticate'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    post 'authenticate', to: 'authentication#authenticate'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end

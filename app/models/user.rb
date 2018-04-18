54c50865875e203971c76ddf96f88bbd1a5d503f
Quentin Sonrel <sudiukil@gmx.fr>
2018-04-18 15:16:20 +0200

    Added validations for User email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+# EmailValidator class, for validating User email
+# @author Quentin Sonrel
+class EmailValidator < ActiveModel::EachValidator
+  # Validate attributes (mandatory from EachValidator)
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << (options[:message] || "is not an email") unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+end
+
 # User class, describes an authentifiable user for the API
 # @author Quentin Sonrel
 class User < ApplicationRecord
@@ -5,4 +14,7 @@ class User < ApplicationRecord
 
   has_many :sources
   has_many :tags
+
+  validates :email, presence: true
+  validates :email, email: true
 end

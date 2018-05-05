6b425fb4e3a68365a1f48e1ce9c57d110d513bb9
Sudiukil <sudiukil@gmx.fr>
2018-05-05 20:30:08 +0200

    Updated User model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'validations' do
-    assert(users(:valid).valid?)
+  test 'validations valid' do
+    1.upto(100) { |i| assert(users("user_#{i}").valid?) }
+  end
+
+  test 'validations attribute presence' do
     assert_not(users(:missing_email).valid?)
+  end
+
+  test 'validation email validity' do
     assert_not(users(:invalid_email).valid?)
   end
 end

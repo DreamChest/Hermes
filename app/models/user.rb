9c5796caff8e77b57b9467d647f96f3d7482c0eb
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-09 16:16:53 +0200

    Added articles access from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
 
   has_many :sources
   has_many :tags
+  has_many :articles, through: :sources
 
   validates :email, presence: true, email: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(email: 'admin@foobar.com', password: 'admin', password_confirmation: 'admin')
-user2 = User.create(email: 'user@foobar.com', password: 'user', password_confirmation: 'user')
+u1 = User.create(email: 'admin@foobar.com', password: 'admin', password_confirmation: 'admin')
+u2 = User.create(email: 'user@foobar.com', password: 'user', password_confirmation: 'user')
 
-source1 = Source.new(name: 'Korben', url: 'https://korben.info/feed')
-source2 = Source.new(name: 'Slashdot', url: 'http://rss.slashdot.org/Slashdot/slashdotMain')
-source3 = Source.new(name: 'Atom Packages', url: 'https://atom.io/packages.atom')
+s1 = u1.sources.create(name: 'Korben', url: 'https://korben.info/feed')
+s2 = u1.sources.create(name: 'Slashdot', url: 'http://rss.slashdot.org/Slashdot/slashdotMain')
+s3 = u2.sources.create(name: 'Atom Packages', url: 'https://atom.io/packages.atom')
 
-user1.sources << source1
-user1.sources << source2
-user2.sources << source3
-
-source1.tag(%w[tech blog])
-source2.tag(['tech'])
-source3.tag(['tech'])
+s1.tag(%w[tech blog])
+# s2.tag(['tech'])
+# s3.tag(['tech'])

b063881900a20afa131ee49253e669bcf9fda0d9
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-09 12:42:27 +0200

    Added Article model testing

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validations valid' do
+    1.upto(50) do |i|
+      assert(articles("article_#{i}").valid?)
+    end
+  end
 end

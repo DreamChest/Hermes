fea15fcb9864d6ef116bdf033f49d70b9233830a
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-25 16:25:15 +0200

    Minor code improvement for Tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,12 +1,3 @@
-# ValidNameValidator class, for validating Tag name
-# @author Quentin Sonrel
-class ValidNameValidator < ActiveModel::EachValidator
-  # Validate attributes (mandatory from EachValidator)
-  def validate_each(record, attribute, value)
-    record.errors[attribute] << 'must have a different value' if value == 'clean'
-  end
-end
-
 # Tag class, describes a tag (categorization for Sources)
 # @author Quentin Sonrel
 class Tag < ApplicationRecord

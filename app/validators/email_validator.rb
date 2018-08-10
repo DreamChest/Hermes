33a15c7735029f4e6644e2c74af8d4ab8d424786
Quentin Sonrel <sudiukil@gmx.fr>
2018-08-10 12:55:09 +0200

    Email validator fix

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -5,6 +5,6 @@ class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     error = (options[:message] || 'is not an email')
     regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    record.errors[attribute] << error unless value.match?(regex)
+    record.errors[attribute] << error unless value =~ regex
   end
 end

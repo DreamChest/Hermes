0c0f7543ba97730e083727f8803db41323e9c14c
Sudiukil <sudiukil@gmx.fr>
2018-08-15 16:49:12 +0200

    Changed article tags filtering method

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,9 +15,6 @@ class Article < ApplicationRecord
   end)
 
   scope :with_tags, (lambda do |tags|
-    joins('
-      inner join sources_tags on articles.source_id = sources_tags.source_id
-      inner join tags on sources_tags.tag_id = tags.id
-    ').where('tags.name in (?)', tags).distinct
+    joins(:tags).where('tags.name in (?)', tags).distinct
   end)
 end
